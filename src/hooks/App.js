207816b51f5a411da19de287376dc7a1d2bede76
Fernando Gomez de Paz <gdp.fernando@gmail.com>
2020-01-01 16:48:05 -0300

    227 - Primer hook en practica, useState

---
+++ src/hooks/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Seconds from './../NoHooks/Seconds';
+
+const App = () => {
+
+    /*
+    state = {
+        showTimer: false
+    };
+
+    setShowTimer = showTimer => {
+        this.setState({showTimer})
+    }*/
+    
+    //const { setShowTimer } = this;
+    //const { showTimer } = this.state;
+
+    const [showTimer, setShowTimer] = useState(false); 
+
+    return (
+        <div className="App">
+            <h1>Aprender hooks (Version con hooks)</h1>
+            <button onClick={() => setShowTimer(!showTimer)}>
+                {!showTimer
+                    ? 'Activar cronómetro'
+                    : 'Apagar cronómetro'}
+            </button>
+            {!showTimer
+                ? (
+                    <div className="alert alert-danger">
+                        Apagado
+                </div>
+                )
+                : (
+                    <div className="alert alert-success">
+                        Activo! <Seconds /> segundos
+                </div>
+                )
+            }
+        </div>
+    );
+}
+
+export default App;
